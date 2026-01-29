393263882bb3befb970c7318dac886b29a99b1dd
binaryash <113206462+binaryash@users.noreply.github.com>
2026-01-29 13:57:36 +0530

    update: mongodb setup comments

--- app/extensions.py
+++ app/extensions.py
@@ -1,7 +1,6 @@
 from pymongo import MongoClient
 
 # Setup MongoDB connection
-# Connects to the local Docker or System Mongo instance
 client = MongoClient('mongodb://localhost:27017/')
 
 # Define the Database and Collection
