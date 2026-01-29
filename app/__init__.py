724cddcabdbb7aca898f47c0d43c9b8eacfed32d
binaryash <113206462+binaryash@users.noreply.github.com>
2026-01-29 14:00:39 +0530

    update: run code for flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,10 @@
 from flask import Flask
-
 from app.webhook.routes import webhook
 
-
-# Creating our flask app
 def create_app():
-
     app = Flask(__name__)
     
-    # registering all the blueprints
+    # Register the blueprint
     app.register_blueprint(webhook)
     
     return app
